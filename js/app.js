3d3b07a3072bfa872d9b997737d6ba7b1b5cc030
Georgios Tzourmpakis <kiougar@gmail.com>
2014-09-10 23:01:47 +0300

    New extension
    Added two more commands and cleaned up the code. Now the add/rem
    commands will only work if there is something to add/rem.

--- js/app.js
+++ js/app.js
@@ -1,6 +1,31 @@
 chrome.runtime.onMessage.addListener( function(request, sender, sendResponse){
     var click_event = new Event('click');
-    
+	
+	/**
+		Checks whether the elem has a class named cname
+	*/
+    var hasClass = function(elem, cname){
+		return elem.classList.contains(cname);
+	}
+	
+	/** 
+		Clicks the element with the given id 
+		when it DOESN'T have the active class.
+		If reverse is true, it clicks it when  
+		it DOES HAVE the active class
+	*/
+	var activateElem = function(id, reverse) {
+		var btn = document.getElementById(id);
+		var isActive = hasClass(btn, "active");
+		if (reverse) {
+			isActive = !isActive;
+		}
+		// activate only if not active
+		if (!isActive) {
+			btn.click();
+		}
+	}
+	
     switch(request.action){
         case 'NEXT-MK':
             //NEXT_MK
@@ -12,15 +37,19 @@ chrome.runtime.onMessage.addListener( function(request, sender, sendResponse){
             break;
         case 'PLAY-PAUSE-MK':
             //PLAY_MK
-            document.getElementById('play-pause').dispatchEvent(click_event);
+            document.getElementById('play-pause').click();
             break;
-        case 'NP-FAV':
-            //NP_FAV
-            document.getElementById('np-fav').click(); // dispatchEvent doesn't work!
+        case 'NP-ADD-FAV':
+			activateElem('np-fav');
+			break;
+		case 'NP-REM-FAV':
+			activateElem('np-fav', true);
+			break;
+        case 'NP-ADD-COL':
+			activateElem('np-add');
             break;
-        case 'NP-ADD':
-            //NP_ADD
-            document.getElementById('np-add').click(); // dispatchEvent doesn't work!
+        case 'NP-REM-COL':
+			activateElem('np-add', true);
             break;
     }
 });
